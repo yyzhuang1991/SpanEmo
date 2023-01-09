4a72a18e0fdb979d8b19cec77c917b9d2a20826d
yyzhuang1991 <u1141153@utah.edu>
2023-01-08 23:54:47 -0700

    fix bug

--- scripts/test.py
+++ scripts/test.py
@@ -18,6 +18,7 @@ from torch.utils.data import DataLoader
 import torch
 from docopt import docopt
 import numpy as np
+import json 
 
 
 args = docopt(__doc__)
@@ -33,16 +34,30 @@ else:
 #####################################################################
 # Define Dataloaders
 #####################################################################
-test_dataset = PredictDataClass(args, args['--test-path'], 1)
+
+test_dataset_with_prev = PredictDataClass(args, args['--test-path'], include_prev_sentence = 1)
+test_data_loader_with_prev = DataLoader(test_dataset_with_prev,
+                              batch_size=int(args['--test-batch-size']),
+                              shuffle=False)
+
+test_dataset = PredictDataClass(args, args['--test-path'], include_prev_sentence = 0)
 test_data_loader = DataLoader(test_dataset,
                               batch_size=int(args['--test-batch-size']),
                               shuffle=False)
-print('The number of Test batches: ', len(test_data_loader))
+
+print('The number of Test batches: ', len(test_data_loader_with_prev))
 #############################################################################
 # Run the model on a Test set
 #############################################################################
 model = SpanEmo(lang=args['--lang'])
+
+learn = Predictor(model, test_data_loader_with_prev, model_path='models/' + args['--model-path'])
+pred = learn.predict(device=device)
+with open(args['--test-path'] + ".out.withprev.json", "w") as f:
+    json.dump(pred, f)
+
+
 learn = Predictor(model, test_data_loader, model_path='models/' + args['--model-path'])
 pred = learn.predict(device=device)
-print(pred)
-
+with open(args['--test-path'] + ".out.json", "w") as f:
+    json.dump(pred, f)
